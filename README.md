77b647102c21f38e8dccbf82ea1bad38c3236868
m4-z3 <melissajzhang@gmail.com>
2019-09-08 03:30:00 -0500

    basic website (with searching) is complete

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HowdyHackers2019
 Created for HowdyHack 2019
+A Better StuAct: C-ORG
--- app.py
+++ app.py
@@ -1,18 +1,123 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, make_response, request, redirect
+import json
 
 app = Flask(__name__)
+org_json = "organizations.json"
 
-@app.route("/")
-def ex():
-    return render_template("layout.html")
+def search(search_words):  # inefficient search
+    #takes in a string that the user puts
+    search_words = search_words.upper()  # turns words to upper case to prevent formatting issues
+    search_words = search_words.split(' ')  # splits words apart at spaces
+    with open(org_json) as file:
+        org_data = json.load(file)
+    
+    # dictionaries which keep track of the org names and the amount of matches to the search
+    name_list = {}
+    tag_list = {}
 
-@app.route("/home")
+    # first searching through the organizations name
+    for i in search_words:
+        for key in org_data.keys():
+            if i in key.split('-'):
+                if key not in name_list:
+                    name_list[key] = 1  # puts counter to one
+                else:
+                    name_list[key] += 1 # adds one to counter since match occurred
+
+    # then searching through tags
+    for i in search_words:
+        for key, value in org_data.items():
+            if i in value["tags"]:
+                if key not in name_list and key not in tag_list:  # preventing the inclusion of orgs that are already in name_list
+                    tag_list[key] = 1
+                elif key in tag_list:
+                    tag_list[key] += 1
+
+    # create list for org names arranged in priority of name (from greatest value to least)  and tags (from greatest to least)
+    final_list =[]
+    for key, value in name_list.items():
+        for i in range(len(search_words), 0, -1):
+            if i == value:
+                final_list.append(key)
+    for key, value in tag_list.items():
+        for i in range(len(search_words), 0, -1):
+            if i == value:
+                final_list.append(key)
+    
+    return final_list
+                
+
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("home.html")
 
-@app.route("/organization")
+@app.route("/about", methods=["GET", "POST"])
+def about():
+    return render_template("about.html")
+
+@app.route("/organizations")
 def organization():
-    return render_template("organization.html")
+    with open(org_json) as file:
+        org_data = json.load(file)
+
+    return render_template("list_organizations.html", org_data = org_data)
+
+# for testing purposes
+# @app.route("/single")
+# def org():
+#     return render_template("organization.html")
+
+# Basic idea for API:
+#   get and post request for search
+#   get request with organization name in title (most likely will have to be exact name)
+#   post request (if adding functionality for organization accounts) which handles adding new info to database
+#   post request which updates organization info
+
+@app.route("/search", methods=["GET", "POST"])
+def org_search():
+    if request.method == "POST":
+        print("search post")
+        search_words = request.form["search"]
+        print("#2")
+        with open(org_json) as file:
+            org_date = json.load(file)
+        org_endpoint = search(search_words)
+        org_list = [org_date[i]["name"] for i in org_endpoint]
+        print("#3")
+        return render_template("search.html", org_list = org_list, org_endpoint = org_endpoint)
+    return render_template("search.html")
+
+@app.route("/organizations/<org_name>", methods=["GET"])
+def org_info(org_name):
+    try:
+        org_name = org_name.upper()
+        with open(org_json) as file:
+            org_data = json.load(file)
+            
+        title = org_data[org_name]["name"]
+        abbreviation = org_data[org_name]["abbrev"]
+        description = org_data[org_name]["description"]
+        contact = org_data[org_name]["contact"]
+        website = org_data[org_name]["website"]
+        level = org_data[org_name]["level"]
+        dues = org_data[org_name]["dues"]
+        size = org_data[org_name]["size"]
+        tags = org_data[org_name]["tags"]
+        if abbreviation != "":
+            return render_template("organization.html", title=title, abbreviation=abbreviation, description=description, contact=contact, website=website, level=level, dues=dues, size=size, tags=tags)
+        else:
+            return render_template("organization.html", title=title, description=description, contact=contact, website=website, level=level, dues=dues, size=size, tags=tags)
+    except:
+        return make_response(render_template("not_found.html"), 404)
+
+# Will be implemented once authorization is implemented
+# @app.route("/organizations/<org_id>/create", methods=["POST"])
+# def org_create(org_id):
+#     return make_response(200)
+
+# @app.route("/organizations/<org_id>/update", methods=["POST"])
+# def org_update(org_id):
+#     return make_response(200)
 
 if __name__ == "__main__":
     app.run(debug = True)
